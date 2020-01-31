650d88d43243381e986ae8724e133c4e7ea04d5f
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-31 22:06:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(songname,songartist)
+    name = songname
+    artist_name = songartist
+  end
+
   def self.all
     @@all
   end
